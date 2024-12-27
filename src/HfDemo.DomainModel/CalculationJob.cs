004bdd38f59b4594b76ab835cc5f059f8957dbfe
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2024-12-27 17:50:11 +0300

    update

--- src/HfDemo.DomainModel/CalculationJob.cs
+++ src/HfDemo.DomainModel/CalculationJob.cs
@@ -2,6 +2,12 @@ namespace HfDemo.DomainModel;
 
 public class CalculationJob
 {
+    private readonly List<CalculationProgress> _progress = [];
+
+    private readonly List<CalculationJob> _children = [];
+
+    private readonly List<CalculationResult> _results = [];
+
     public Guid Id { get; set; }
 
     public Guid? ParentId { get; set; }
@@ -18,9 +24,59 @@ public class CalculationJob
 
     public string ParametersJson { get; set; } = string.Empty;
 
-    public IEnumerable<CalculationJob> ChildJobs { get; set; } = [];
+    public IEnumerable<CalculationJob> ChildJobs => _children.AsReadOnly();
 
-    public IEnumerable<CalculationProgress> Progress { get; set; } = [];
+    public IEnumerable<CalculationProgress> Progress => _progress.AsReadOnly();
 
-    public IEnumerable<CalculationResult> Results { get; set; } = [];
+    public IEnumerable<CalculationResult> Results => _results.AsReadOnly();
+
+
+    public void AppendProgress(int percent, string? message = null)
+    {
+        var progress = new CalculationProgress
+        {
+            Id = Guid.NewGuid(),
+            CreationTime = DateTime.UtcNow,
+            JobId = Id,
+            Prercent = percent,
+            Message = message
+        };
+
+        _progress.Add(progress);
+    }
+
+    public CalculationJob CreateChildJob(string name, string type, string parameters)
+    {
+        var job = new CalculationJob
+        {
+            Id = Guid.NewGuid(),
+            ParentId = Id,
+            Name = name,
+            Type = type,
+            StartTime = DateTime.UtcNow,
+            Status = JobStatus.Unknown,
+            ParametersJson = parameters
+        };
+
+        _children.Add(job);
+        return job;
+    }
+
+    public void SetResult(string name, string type, string resultJson, string? metadataJson, bool isCompleted, string? message)
+    {
+        var result = new CalculationResult
+        {
+            Id = Guid.NewGuid(),
+            CreationTime = DateTime.UtcNow,
+            JobId = Id,
+            Name = name,
+            Type = type,
+            ResultJson = resultJson,
+            MetadataJson = metadataJson,
+            IsCompleted = isCompleted,
+            Message = message
+        };
+
+        _results.Add(result);
+    }
 }
