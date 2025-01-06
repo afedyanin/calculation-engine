4080b0716250e99fa55e2d865237fc3eb31a2e61
Anatoly Fedyanin <anfemail@gmail.com>
2025-01-06 14:33:57 +0300

    update

--- tests/CalculationEngine.Core.Tests/HangfireExtensions/BacgroundJobClientExtensionsTests.cs
+++ tests/CalculationEngine.Core.Tests/HangfireExtensions/BacgroundJobClientExtensionsTests.cs
@@ -21,4 +21,30 @@ public class BacgroundJobClientExtensionsTests : HangfireClientTestBase
 
         Console.WriteLine($"Enqueued job={jobId}");
     }
+
+    [Test]
+    public void CanEnqueueSequenceOfJobs()
+    {
+        var request = new DelayRequest
+        {
+            CorrelationId = Guid.NewGuid(),
+            Delay = TimeSpan.FromSeconds(1),
+        };
+
+        var jobId1 = BackgroundJobClient.EnqueueBackgroundJob(request);
+        var jobId2 = BackgroundJobClient.ContinueWithBackgropundJob(jobId1, request);
+        var jobId3 = BackgroundJobClient.ContinueWithBackgropundJob(jobId2, request);
+        var jobId4 = BackgroundJobClient.ContinueWithBackgropundJob(jobId3, request);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(jobId1, Is.Not.Empty);
+            Assert.That(jobId2, Is.Not.Empty);
+            Assert.That(jobId3, Is.Not.Empty);
+            Assert.That(jobId4, Is.Not.Empty);
+        });
+
+        var jobIds = new string[] { jobId1, jobId2, jobId3, jobId4 };
+        Console.WriteLine($"Enqueued jobs={string.Join(',', jobIds)}");
+    }
 }
