502c5f4edcd8aa3ce367303760987bdef4386923
Anatoly Fedyanin <anfemail@gmail.com>
2025-01-08 22:48:30 +0300

    update

--- tests/CalculationEngine.Graphlib.Tests/Algos/TopologicalSortingTests.cs
+++ tests/CalculationEngine.Graphlib.Tests/Algos/TopologicalSortingTests.cs
@@ -1,3 +1,6 @@
+using CalculationEngine.Graphlib.Algos;
+using static CalculationEngine.Graphlib.Tests.GraphFactory;
+
 namespace CalculationEngine.Graphlib.Tests.Algos;
 
 [TestFixture(Category = "Unit")]
@@ -6,6 +9,46 @@ public class TopologicalSortingTests
     [Test]
     public void CanSortSimpleGraph()
     {
+        var graph = new Graph<Item>();
+        var v1 = graph.AddVertex(new Item(40));
+        var v2 = graph.AddVertex(new Item(50));
+        var v3 = graph.AddVertex(new Item(60));
 
+        graph.AddEdge(v3, v2);
+        graph.AddEdge(v2, v1);
+
+        var sorted = graph.TopologicalSort();
+
+        var actual = string.Join(',', sorted.Select(x => x.Index + 1));
+        var expected = string.Join(',', 3, 2, 1);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    // https://www.geeksforgeeks.org/topological-sorting/
+    public void CanSortGraph01()
+    {
+        var graph = new Graph<Item>();
+        var v0 = graph.AddVertex(new Item(0));
+        var v1 = graph.AddVertex(new Item(10));
+        var v2 = graph.AddVertex(new Item(20));
+        var v3 = graph.AddVertex(new Item(30));
+        var v4 = graph.AddVertex(new Item(40));
+        var v5 = graph.AddVertex(new Item(50));
+
+        graph.AddEdge(v3, v1);
+        graph.AddEdge(v2, v3);
+        graph.AddEdge(v5, v2);
+        graph.AddEdge(v4, v1);
+        graph.AddEdge(v5, v0);
+        graph.AddEdge(v4, v0);
+
+        var sorted = graph.TopologicalSort();
+
+        var actual = string.Join(',', sorted.Select(x => x.Index));
+        var expected = string.Join(',', 5, 4, 2, 3, 1, 0);
+
+        Assert.That(actual, Is.EqualTo(expected));
     }
 }
